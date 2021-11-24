54b1a4e8d83be1920d792f86102f7f8c09c8c012
unai <unai.diez@ikasle.egibide.org>
2021-11-24 11:57:37 +0100

    cambios js-php v1
    minicambios v1

--- portfolio/php/enviarFormBaseDatos.php
+++ portfolio/php/enviarFormBaseDatos.php
@@ -2,31 +2,51 @@
 
 require 'conexionBBDD.php';
     
-    $formulario=$_POST["formulario"];
+    //$formulario=$_POST["formulario"];
+    /*echo $formulario;
     if($formulario=="usuario"){
-        crearUsuario();
-    }else{
+       */ crearUsuarios();
+    /*}else{
         crearProducto();
-    }
+    }*/
 
 
-function crearUsuario(){
-    $contrasena=hash("sha256",$_POST["contrasena"]);
-    echo $contrasena;
+function crearUsuarios(){
     $datos= array(
-        'nombreUsuario'=> $_POST["nombreUsuario"],
-        'contrasena'=> $contrasena,
+        'nombreUsuario'=>$_POST["nombreUsuario"],
+        'contrasena'=> $_POST["contrasena"],
         'nombre'=>$_POST["nombre"],
         'correo'=>$_POST["correo"],
         'telefono'=>$_POST["telefono"],
         'direccion'=>$_POST["direccion"]
     );
-        
+    //enviarImg();
     $dbh=connect();
+    if (isset($_FILES['imgusu'])) {
+        $image = $_FILES['imgusu']['tmp_name']; 
+        $nombre_inagen=$_FILES['imgusu']['name'];
+        $tipo_inagen = $_FILES['imgusu']['type'];
+        echo $tipo_inagen;
+       //ruta de la carpeta destino del server
+        //$_SERVER['DOCUMENT_ROOT'] ==> /var/www/html (direccion original del localhost del servidor)
+        // $carpeta_destino=$_SERVER['DOCUMENT_ROOT'].'/carpeta de guardado de archivos/';
+        $carpeta_destino= $_SERVER['DOCUMENT_ROOT'].'/imagenes/';
+        //Crear conexion con la abse de datos
+        //Insertar imagen en la base de datos
+        $ficha= array(
+            'archivo'=> $nombre_inagen
+        );
+        $dbd=connect();
+        $insertar = $dbd->prepare("INSERT into imagen (imagenfoto) VALUES (:archivo)");
+        $insertar->execute($ficha);
+        $dbs=close();
+        // COndicional para verificar la subida del fichero
+        move_uploaded_file($image,$carpeta_destino.$nombre_inagen);
+    }
     $stmt= $dbh->prepare("INSERT INTO usuario (nombreusu,contrasenausu,nombre,gmail,tlfno,direccion) VALUES (:nombreUsuario,:contrasena,:nombre,:correo,:telefono,:direccion)");
     $stmt->execute($datos);
     $dbh=close();
-    enviarImg();
+
 }
 
 function crearProducto(){
@@ -45,20 +65,16 @@ function crearProducto(){
 }
 
 function enviarImg(){
-        
-        foreach ($_FILES['foto']['name'] as $key => $value){
-            print_r($_FILES['foto']['name'][$key]);
+        foreach ($_FILES['imgusu']['name'] as $key ){
+            print_r($_FILES['imgusu']['name'][$key]);
        
-            if ( $_FILES['foto']['name'][$key]) {
+            if ($_FILES['imgusu']['name'][$key]) {
                 
-                $image = $_FILES['foto']['tmp_name'][$key]; 
-                $revisar = getimagesize($image);
-                if($revisar !== false){
-                    $nombre_inagen=$_FILES['foto']['name'][$key];
-                    $tipo_inagen = $_FILES['foto']['type'][$key];
+                $image = $_FILES['imgusu']['tmp_name'][$key]; 
+                    $nombre_inagen=$_FILES['imgusu']['name'][$key];
+                    $tipo_inagen = $_FILES['imgusu']['type'][$key];
                     echo $tipo_inagen;
-                    $tamano_inagen = $_FILES['foto']['size'][$key];
-                    //ruta de la carpeta destino del server
+                   //ruta de la carpeta destino del server
     
                     //$_SERVER['DOCUMENT_ROOT'] ==> /var/www/html (direccion original del localhost del servidor)
                     // $carpeta_destino=$_SERVER['DOCUMENT_ROOT'].'/carpeta de guardado de archivos/';
@@ -68,11 +84,11 @@ function enviarImg(){
                     move_uploaded_file($image,$carpeta_destino.$nombre_inagen);
                     
                     //Crear conexion con la abse de datos
-                    $db = connect();
-    
-                    
+                    $dbd = connect();
                     //Insertar imagen en la base de datos
-                    $insertar = $db->query("INSERT into imagen (imagenfoto) VALUES ('$nombre_inagen')");
+
+                    $insertar = $dbd->prepare("INSERT into imagen (imagenfoto, usuario_idusu) VALUES ('$nombre_inagen','10')");
+                    $insertar->execute();
                     // COndicional para verificar la subida del fichero
                     if($insertar){
                         echo "Archivo Subido Correctamente.";
@@ -83,11 +99,11 @@ function enviarImg(){
                         echo "Ha fallado la subida, reintente nuevamente.";
                     } 
                     // Si el usuario no selecciona ninguna imagen
-                }
+            }
                 else{
                     echo "Por favor seleccione imagen a subir.";
                 }
-            }
+            
         }
     
 }
--- portfolio/php/main/formRegistro.php
+++ portfolio/php/main/formRegistro.php
@@ -1,33 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Registro</title>
+        <script src="../js/jquery-3.6.0.min.js"></script>
+        <script src="../js/objetos.js"></script>
+        <script src="../js/verificarFormularios.js"></script>
+        <script src="../js/script.js"></script>
+        <script src="../js/sideNav.js"></script>
+        <link rel="stylesheet" href="../css/style.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js"></script>
+    </head>
+    <body>
+        <?php
+            require 'header.php'
+        ?>
+        <main>
+        <button class="openbtn" >&#11446;</button> 
+            <form action="registro.php" method="post" id="formUsuario" enctype="multipart/form-data">
+                <fieldset>
+                    <legend>Registrar usuario</legend>
+                    <div class="formulario">
+                        <label for="nombreUsuario">Nombre usuario</label>
+                        <input type="text" class="usuario" id="nombreUsuario" placeholder="pepe12" >
+                        
+                        <label for="contrasena1">Contraseña</label>
+                        <input type="password" class="usuario" id="contrasena1" placeholder="contrasena" >
+                        
+                        <label for="contrasena2">Repetir contraseña</label>
+                        <input type="password" class="usuario" id="contrasena2" placeholder="contrasena" >
+                        
+                        <label for="nombre">Su nombre</label>
+                        <input type="text" class="usuario" id="nombre" placeholder="nombre">
+                        
+                        <label for="correo">Correo electronico</label>
+                        <input type="text" class="usuario" id="correo" placeholder=" pepe@gmail.com">
 
-<form action="registro.php" method="post" id="formUsuario" enctype="multipart/form-data">
-    <fieldset>
-        <legend>Registrar usuario</legend>
-        <div class="formulario">
-            <label for="nombreUsuario">Nombre usuario</label>
-            <input type="text" class="usuario" id="nombreUsuario" placeholder="pepe12" >
-            
-            <label for="contrasena1">Contraseña</label>
-            <input type="password" class="usuario" id="contrasena1" placeholder="contrasena" >
-            
-            <label for="contrasena2">Repetir contraseña</label>
-            <input type="password" class="usuario" id="contrasena2" placeholder="contrasena" >
-            
-            <label for="nombre">Su nombre</label>
-            <input type="text" class="usuario" id="nombre" placeholder="nombre">
-            
-            <label for="correo">Correo electronico</label>
-            <input type="text" class="usuario" id="correo" placeholder=" pepe@gmail.com">
-
-            <label for="tel">Telefono</label>
-            <input type="text" class="usuario" id="tel" placeholder="655633677">
-            
-            <label for="direccion">Direccion</label>
-            <input type="text" class="usuario" id="direccion" placeholder=" Vitoria,alava">
-            
-            <label for="foto[]">Foto</label>
-            <input type="file" class="usuario" id="foto" name="foto[]"  >
-            
-            <input type="button" value="Enviar" id="enviar" class="boton">
-        </div>
-    </fieldset>
-</form>
+                        <label for="tel">Telefono</label>
+                        <input type="text" class="usuario" id="tel" placeholder="655633677">
+                        
+                        <label for="direccion">Direccion</label>
+                        <input type="text" class="usuario" id="direccion" placeholder=" Vitoria,alava">
+                        
+                        <label for="foto">Foto</label>
+                        <input type="file" class="usuario" id="imgusu" name="imgusu" >
+                        
+                        <input type="button" value="Enviar" id="enviar" class="boton">
+                    </div>
+                </fieldset>
+            </form>
+        </main>
+        <?php
+            require 'footer.php'
+        ?>
+    </body>
+</html>
